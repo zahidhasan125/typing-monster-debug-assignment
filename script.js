3d32aa87e21f5d466fd0138d630eb8eda21e59fc
zahidhasan125 <zahidhasan125@gmail.com>
2022-09-15 01:48:17 -0400

    Test time show in decimal format

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ const gameOver = () => {
   // show result
   resultModal.innerHTML = `
     <h1>Finished!</h1>
-    <p>You took: <span class="bold">${timeTaken}</span> seconds</p>
+    <p>You took: <span class="bold">${parseInt(timeTaken)}</span> seconds</p>
     <p>You made <span class="bold red">${errorCount}</span> mistakes</p>
     <button onclick="closeModal()">Close</button>
   `;
@@ -133,7 +133,5 @@ displayHistory();
 setInterval(() => {
   const currentTime = new Date().getTime();
   const timeSpent = (currentTime - startTime) / 1000;
-
-
-  document.getElementById("show-time").innerHTML = `${startTime ? timeSpent : 0} seconds`;
+  document.getElementById("show-time").innerHTML = `${startTime ? parseInt(timeSpent) : 0} seconds`;
 }, 1000);
